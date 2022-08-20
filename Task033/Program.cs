ed418a89651c2b40ae3a351af4caa978ccb36e35
Ibragim.Ast <ibragim.ast@gmail.com>
2022-08-21 00:39:26 +0300

    Add solution Task 033

---
+++ Task033/Program.cs
@@ -0,0 +1,35 @@
+﻿int[] CreateArray(int length, int min, int max)
+{
+    int[] array = new int[length];
+    for (int i = 0; i < length; i++)
+    {
+        array[i] = new Random().Next(min, max+1);
+    }
+    return array;
+}
+
+void PrintArray(int[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        Console.Write($"{array[i]} ");
+    }
+    Console.WriteLine();
+}
+
+void FindNumberInArray(int[] array, int x)
+{
+    int result = 0;
+    for (int i = 0; i < array.Length; i++)
+        {
+        if (array[i] == x)
+            result = i; 
+        else
+            result = -1;           
+        }
+        Console.WriteLine($"Число {x} находится под индексом {result}");
+}
+
+int[] array = CreateArray(10, -10, 10);
+PrintArray(array);
+FindNumberInArray(array, -4);
