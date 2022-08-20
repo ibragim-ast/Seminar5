6ab001116867eef2be69260c4f638185b85faef8
Ibragim.Ast <ibragim.ast@gmail.com>
2022-08-21 01:44:44 +0300

    Add Task 035 & solution

---
+++ Task035/Program.cs
@@ -0,0 +1,21 @@
+﻿int[] CreateArray123(int length, int min, int max)
+{
+    int[] array = new int[length];
+    for (int i = 0; i < length; i++)
+    {
+        array[i] = new Random().Next(min, max+1);
+    }
+    return array;
+}
+
+void PrintArray(int[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        Console.Write($"{array[i]} ");
+    }
+    Console.WriteLine();
+}
+
+int[] array = CreateArray123(123, 10, 100);
+PrintArray(array);
