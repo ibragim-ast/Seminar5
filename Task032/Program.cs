405d5e6cd6e0a91171aeaa17d3d7d9db0cd62b12
Ibragim.Ast <ibragim.ast@gmail.com>
2022-08-20 21:32:04 +0300

    add solution Task 032

---
+++ Task032/Program.cs
@@ -0,0 +1,36 @@
+﻿int[] CreateArray(int length, int min, int max)
+{
+    int[] array = new int[length];
+    for (int i = 0; i < length; i++)
+    {
+        array[i] = new Random().Next(min, max+1);
+    }
+    return array;
+}
+
+void PrintArray(int[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        Console.Write($"{array[i]} ");
+    }
+    Console.WriteLine();
+}
+
+void ReplaceNegativePozitive(int[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+        {
+        if (array[i] > 0)
+            array[i] = -array[i];
+        else
+            array[i] *= -1; 
+        }
+}
+
+int[] array = CreateArray(4, -9, 9);
+PrintArray(array);
+ReplaceNegativePozitive(array);
+PrintArray(array);
+
+
