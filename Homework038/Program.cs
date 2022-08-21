1f08492a6a1736b36d4632e66e1b1bc7556f9446
Ibragim.Ast <ibragim.ast@gmail.com>
2022-08-21 12:48:48 +0300

    Add Homework038 solution

---
+++ Homework038/Program.cs
@@ -0,0 +1,40 @@
+﻿
+double[] CreateArray(int length)
+{
+    double[] array = new double[length];
+    for (int i = 0; i < length; i++)
+    {
+        array[i] = new Random().NextDouble();
+    }
+    return array;
+}
+
+void PrintArray(double[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        Console.Write($"{Math.Round(array[i], 2)} ");
+    }
+    Console.WriteLine();
+}
+
+void DifferenceMinMax(double[] array)
+    {
+        int length = array.Length;
+        double min = array[0];
+        double max = array[0];
+        double result = 0;
+        for (int i = 0; i < length; i++)
+        {
+            if (min > array[i])
+                min = array[i]; 
+            if (max < array[i])
+                max = array[i];
+        }
+        result = max - min;
+        Console.WriteLine($"Разница между максимальным и минимальным элементами массива равна {Math.Round(result, 2)}");
+    }
+
+double[] array = CreateArray(10);
+PrintArray(array);
+DifferenceMinMax(array);
